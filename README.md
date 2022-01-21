728fbb9cff126afca18fcd2db894b1c7556086d3
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-20 20:52:32 -0600

    starting working on front-end

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 
 REFERENCES
 
+//Front-End Resources 
 https://chakra-ui.com/docs/getting-started
+https://www.npmjs.com/package/react-router-dom
+https://chakra-ui.com/docs/media-and-icons/icon
+
+
---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Home() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <div>
+      Welcome to Play It Again Tunes!
+    </div>
+  )
+}
+
+export default Home;
+
+// TODO FRONT-END 
+// Need to re-evaulate home.js
