8b4fd5e297f5b02a42170348edb027be41e07303
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-28 02:33:25 -0600

    cont working on front end

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Heading, Text ,Link} from '@chakra-ui/react'
 
 
 export const About = () => {
@@ -16,14 +16,13 @@ export const About = () => {
           About Us
         </Heading>
         <Text mt="4" fontSize="lg">
-          Play It Again Tunes is a demonstration of what the Wholesome 4 is capable of after learning Full Stack Web Development from the SMU Coding Bootcamp. Alice, Charlie, Miranda and Mike worked together to build a website where students and educators can rent musical instruments and equipment.
-
-          The Wholesome 4 were inspired to build Play It Again Tunes by their collective love for music and Charlie’s hands-on experience as a high school educator.
-
-          Miranda and Mike both work in the insurance industry but are eager to work more closely with software development and find a career path that includes writing code.
-          
-          Alice is a receptions and an aspiring back-end software developer. She hopes to seek employment as a junior developer after Coding Bootcamp.
-          The Wholesome 4 currently reside in the Dallas-Fort Worth Metroplex but grew up in different cities, states and even countries. For business inquiries, email us at mike@mallonit.com
+           Play It Again Tunes is a demonstration of what the Wholesome 4 is capable of after learning Full Stack Web Development from the SMU Coding Bootcamp. Alice, Charlie, Miranda and Mike worked together to build a website where students and educators can rent musical instruments and equipment.
+        <br></br><br></br>
+        The Wholesome 4 were inspired to build Play It Again Tunes by their collective love for music and Charlie’s hands-on experience as a high school educator.
+        <br></br><br></br>
+        Miranda and Mike both work in the insurance industry but are eager to work more closely with software development and find a career path that includes writing code. Alice is a receptions and an aspiring full stack software developer. She hopes to seek employment as a junior developer after Coding Bootcamp.
+          <br></br><br></br>
+          The Wholesome 4 currently reside in the Dallas-Fort Worth Metroplex but grew up in different cities, states and even countries. For business inquiries, <Link href='/contact' rel='noopener noreferrer'>Click Here!</Link>
         </Text>
       </Box>
     </Box>
